04e24570bf7c037fb4a16e452d5765d94cc0876e
youssef <yousseflatr65@gmail.com>
2025-12-06 13:00:59 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
         <div>
-           <BrowserRouter>
+           <BrowserRouter basename="/PORTFOLIO">
                 <Header/>
             </BrowserRouter>
         </div>
